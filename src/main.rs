7fe2716a99cda360c87bbcf223a2a0940f60e569
Niko Kivel <niko.kivel@lightsoure.ce>
2023-04-03 15:08:00 -0600

    install sub command isolated

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,14 @@
-use std::env;
-use std::fs;
-use std::path::Path;
-
 // for CLI
 use clap::Parser;
 
 // logging
 use colored::Colorize;
-use config::Config;
-use log::{debug, error, info, trace, warn};
+use log::debug;
 use simple_logger::SimpleLogger;
 
 // my mods
 mod diff;
 pub mod ioc;
-use ioc::IOC;
 
 pub mod cli;
 use cli::{Cli, Commands};
@@ -25,44 +19,9 @@ use settings::Settings;
 mod metadata;
 use metadata::PackageData;
 
+mod install;
+
 pub mod log_macros;
-use crate::log_macros::{cross, exclaim, tick};
-
-fn collect_iocs(
-    ioc_names: &[String],
-    stage_root: impl AsRef<Path>,
-    destination_root: impl AsRef<Path>,
-) -> Vec<IOC> {
-    let mut iocs: Vec<IOC> = Vec::new();
-    debug!("collecting iocs ...");
-    ioc_names.iter().for_each(|name| {
-        let work_dir = env::current_dir().unwrap().join(name);
-        trace!("working dir: {:?}", work_dir);
-        match IOC::new(&work_dir, &stage_root, &destination_root) {
-            Ok(new_ioc) => iocs.push(new_ioc),
-            Err(e) => error!(
-                "{} IOC::new failed for <{}> with: {}",
-                cross!(),
-                name.red().bold(),
-                e
-            ),
-        };
-    });
-    iocs
-}
-
-fn ioc_cleanup(ioc: &IOC) -> std::io::Result<()> {
-    trace!("cleaning up staging directory for {}", &ioc.name);
-    fs::remove_dir_all(&ioc.stage)?;
-    info!("{} cleaning up: removed {:?}", tick!(), &ioc.stage);
-    Ok(())
-}
-
-fn remove_dir(dir: impl AsRef<Path>) -> std::io::Result<()> {
-    trace!("removing directory {}", dir.as_ref().to_str().unwrap());
-    fs::remove_dir_all(dir)?;
-    Ok(())
-}
 
 fn main() {
     let cli = Cli::parse();
@@ -80,26 +39,9 @@ fn main() {
         .unwrap();
 
     let crate_info = PackageData::new();
-    trace!("metadata --------------------------------");
-    trace!("  name   : {}", crate_info.get_name());
-    trace!("  desc   : {}", crate_info.get_description());
-    trace!("  version: {}", crate_info.get_version());
-    trace!("  authors: {}", crate_info.get_authors());
-    trace!("  repo:    {}", crate_info.get_repository());
-    trace!("-----------------------------------------");
-
-    let stage_root = settings.get::<String>("filesystem.stage").unwrap();
-    let deploy_root = settings.get::<String>("filesystem.deploy").unwrap();
-    let template_dir = settings.get::<String>("app.template_directory").unwrap();
-
-    trace!("configuration ---------------------------");
-    trace!("  stage:    {:?}", stage_root);
-    trace!("  deploy:   {:?}", deploy_root);
-    trace!("  templates:{:?}", template_dir);
-    trace!("-----------------------------------------");
+    crate_info.report();
 
     // CLI commands
-    // TODO: terribly nested, check for way to flatten
     match &cli.command {
         Some(Commands::Install {
             dryrun,
@@ -113,114 +55,9 @@ fn main() {
             debug!("retain: {}", retain);
             debug!("no diff: {}", nodiff);
             debug!("force:  {}", force);
-            let ioc_list = match iocs {
-                Some(i) => collect_iocs(i, &stage_root, &deploy_root),
-                None => panic!(),
-            };
-            trace!("{} ioc list created", tick!());
             // worker
-            // TODO: move to function
-            for ioc in &ioc_list {
-                info!("----- {} -----", ioc.name.blue().bold());
-                trace!("{:?}", ioc);
-                // temper check
-                match ioc.check_hash() {
-                    Ok(hash) => {
-                        info!("{} valid hash for {} |{}|", tick!(), &ioc.name.blue(), hash);
-                    }
-                    Err(e) => {
-                        if !force {
-                            error!("{} {} --> check destination <{:?}> and use `{} {}` to deploy regardless", cross!(), e, &ioc.destination.as_path(), "ioc install --force".yellow(), &ioc.name.yellow());
-                            continue;
-                        } else {
-                            warn!(
-                                "{} failed hash check overwritten by {}",
-                                exclaim!(),
-                                "--force".yellow()
-                            );
-                        }
-                    }
-                }
-                // staging
-                trace!("staging {}", ioc.name.blue().bold());
-                perform_stage_deploy(&settings, ioc, nodiff);
-                // deployment
-                if !dryrun {
-                    trace!("deploying {}", ioc.name.blue().bold());
-                    match ioc.deploy() {
-                        Ok(_) => info!("{} deployed {}", tick!(), ioc.name.blue()),
-                        Err(e) => error!(
-                            "{} deployment of {} failed with: {}",
-                            cross!(),
-                            ioc.name.red().bold(),
-                            e
-                        ),
-                    };
-                    match ioc_cleanup(ioc) {
-                        Ok(_) => {}
-                        Err(e) => error!(
-                            "{} clean up failed for {} with error: {}",
-                            cross!(),
-                            &ioc.name,
-                            e
-                        ),
-                    };
-                    match remove_dir(Path::new(&stage_root)) {
-                        Ok(_) => info!("{} stage root removed", tick!()),
-                        Err(e) => {
-                            error!("{} failed to remove stage root with error: {}", cross!(), e)
-                        }
-                    };
-                } else {
-                    info!("{} was chosen, no deployment", "--dryrun".yellow());
-                    if !retain {
-                        match ioc_cleanup(ioc) {
-                            Ok(_) => {}
-                            Err(e) => error!(
-                                "{} clean up failed for {} with error: {}",
-                                cross!(),
-                                &ioc.name,
-                                e
-                            ),
-                        };
-                    } else {
-                        info!(
-                            "{} stage directory retained. Make sure to clean up after yourself!",
-                            exclaim!()
-                        );
-                    }
-                }
-                trace!("------------");
-            }
+            install::ioc_install(iocs, &settings, dryrun, retain, nodiff, force);
         }
-        None => println!("NO ACTION --> BYE"),
-    }
-}
-
-// fn perform_deployment(ioc: &mut IOC) {}
-
-fn perform_stage_deploy(settings: &Config, ioc: &IOC, nodiff: &bool) {
-    trace!("staging {}", ioc.name.blue().bold());
-    let template_dir = settings.get::<String>("app.template_directory").unwrap();
-    match ioc.stage(template_dir.as_str()) {
-        // match ioc.stage(conf.template_dir.as_str()) {
-        Ok(_) => info!("{} staged {}", tick!(), ioc.name.blue()),
-        Err(e) => error!(
-            "{} staging of {} failed with: {}",
-            cross!(),
-            ioc.name.red().bold(),
-            e
-        ),
-    }
-    if ioc.destination.exists() && !nodiff {
-        match ioc.diff_ioc() {
-            Ok(_) => info!("{} diffed {} see output above", tick!(), ioc.name.blue()),
-            Err(e) => error!(
-                "{} diff of {} failed with: {}",
-                cross!(),
-                ioc.name.red().bold(),
-                e
-            ),
-        }
+        None => println!("no active command, check --help for more information."),
     }
 }
