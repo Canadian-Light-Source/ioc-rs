503c794ba5e3b95767fbc7162ccd13ad6218eae4
Niko Kivel <niko.kivel@lightsoure.ce>
2023-04-04 11:17:36 -0600

    test for diff

--- src/diff.rs
+++ src/diff.rs
@@ -5,7 +5,6 @@ use diffy::{create_patch, PatchFormatter};
 // fn get_patch()
 
 pub fn get_patch<P>(original: P, modified: P) -> io::Result<String>
-// fn get_patch<P>(original: P, modified: P) -> io::Result<Patch<'static, str>>
 where
     P: AsRef<Path>,
 {
@@ -16,4 +15,20 @@ where
     let f = PatchFormatter::new().with_color();
     let s = f.fmt_patch(&patch.to_owned()).to_string();
     Ok(s)
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_hash_directory() {
+        let original = Path::new("./tests/diff_test/original");
+        let modified = Path::new("./tests/diff_test/modified");
+        let expected = "\u{1b}[1m--- original\n+++ modified\n\u{1b}[0m\u{1b}[36m@@ -1 +1 @@\u{1b}[0m\n\u{1b}[31m-this is the original version\n\u{1b}[0m\u{1b}[32m+this is the modified version\n\u{1b}[0m";
+
+        let patch = get_patch(original, modified);
+        assert_eq!(patch.unwrap(), expected);
+    }
 }
