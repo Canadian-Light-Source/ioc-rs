edf49c94023419024d0a651859c53a96a3a7f3e1
Niko Kivel <niko.kivel@lightsoure.ce>
2023-04-05 16:41:26 -0600

    test for stage.rs

--- src/stage.rs
+++ src/stage.rs
@@ -43,7 +43,7 @@ pub fn ioc_stage(ioc_name: &Option<String>, ioc_struct: Option<IOC>, settings: &
     }
 }
 
-pub fn stage(ioc: &IOC, template_dir: &str) -> std::io::Result<()> {
+fn stage(ioc: &IOC, template_dir: &str) -> io::Result<()> {
     trace!("staging {}", ioc.name.blue());
     if ioc.stage.exists() {
         remove_dir_contents(&ioc.stage)?; // prep stage directory
@@ -77,10 +77,7 @@ fn remove_dir_contents<P: AsRef<Path>>(path: P) -> io::Result<()> {
 }
 
 /// Copy files from source to destination recursively.
-fn copy_recursively(
-    source: impl AsRef<Path>,
-    destination: impl AsRef<Path>,
-) -> std::io::Result<()> {
+fn copy_recursively(source: impl AsRef<Path>, destination: impl AsRef<Path>) -> io::Result<()> {
     fs::create_dir_all(&destination)?;
     for entry in fs::read_dir(source)? {
         let entry = entry?;
@@ -99,4 +96,39 @@ fn copy_recursively(
         }
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::settings::Settings;
+
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_stage_ioc() {
+        let settings = Settings::build("tests/config/test.toml").unwrap();
+
+        let test_ioc = IOC::new(
+            Path::new("./tests/MTEST_IOC01"),
+            Path::new("./tests/tmp/stage_test1/"),
+            Path::new("./tests/tmp/deploy/ioc/"),
+        )
+        .unwrap();
+        ioc_stage(&None, Some(test_ioc.clone()), &settings);
+        // the actual check
+        assert!(&test_ioc.stage.exists());
+        assert!(fs::remove_dir_all(&test_ioc.stage).is_ok());
+    }
+
+    #[test]
+    fn test_stage_ioc2() {
+        let settings = Settings::build("tests/config/test.toml").unwrap();
+
+        ioc_stage(&Some("tests/MTEST_IOC01".to_string()), None, &settings);
+        // the actual check
+        let stage_dir = Path::new("./tests/tmp/stage/MTEST_IOC01");
+        assert!(stage_dir.exists());
+        assert!(fs::remove_dir_all(stage_dir).is_ok());
+    }
 }
