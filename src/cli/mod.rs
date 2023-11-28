8e990b0b209d6bc2bf5eaf66fc82a9ae9ab05746
Niko Kivel <niko.kivel@lightsoure.ce>
2023-11-28 13:29:26 -0600

    generate_shell_completion

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -14,7 +14,7 @@ use log::LevelFilter;
 )]
 pub struct Cli {
     /// shell tab complete generator
-    #[arg(long = "generate", value_enum, hide = true)]
+    #[arg(long = "generate_shell_completion", value_enum, hide = true)]
     pub generator: Option<Shell>,
 
     /// display version
