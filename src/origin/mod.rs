0ec8a8e7b49d1a1778d16cc267e0ef404112a14c
Niko Kivel <niko.kivel@lightsoure.ce>
2023-12-04 11:26:37 -0600

    origin refactored to contain more git related info.

--- src/origin/mod.rs
+++ src/origin/mod.rs
@@ -1,21 +1,23 @@
-use git2::Repository;
+use git2::{Repository, RepositoryState};
 use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::Write;
 use std::path::{Path, PathBuf};
 
+use chrono::NaiveDateTime;
+
 /// struct for origin information
-/// directory: mandatory source directory
-/// url: optional for git repositories
-/// branch_name: optional for git repositories
-/// tag: optional for git repositories
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Default, Serialize, Deserialize)]
 pub struct Origin {
     directory: PathBuf,
-    url: String,
-    branch_nane: String,
+    remote: String,
+    branch: String,
     message: String,
+    commit: String,
+    time: String,
     tag: String,
+    author: String,
+    state: String,
 }
 
 impl Origin {
@@ -30,96 +32,58 @@ impl Origin {
     ///
     /// Origin
     pub fn new<P: AsRef<Path>>(dir: P) -> Self {
-        match Self::from_git_dir(&dir).or(Self::from_dir(&dir)) {
-            Ok(o) => o,
-            Err(e) => panic!("{}", e),
+        let mut origin: Origin = Default::default();
+        origin.directory = dir.as_ref().canonicalize().unwrap_or_default();
+
+        if let Ok(repo) = Repository::open(".") {
+            // Print the branch name
+            if let Ok(head) = repo.head() {
+                if let Some(name) = head.shorthand() {
+                    origin.branch = name.to_owned();
+                    println!("Branch: {}", origin.branch);
+                }
+            }
+
+            // Print the last commit details
+            if let Ok(reference) = repo.head() {
+                if let Some(oid) = reference.target() {
+                    if let Ok(commit) = repo.find_commit(oid) {
+                        origin.author = commit.author().name().unwrap_or_default().to_owned();
+                        origin.message = commit.summary().unwrap_or_default().to_owned();
+                        origin.commit = oid.to_string();
+                        origin.time = NaiveDateTime::from_timestamp_opt(commit.time().seconds(), 0)
+                            .unwrap_or_default()
+                            .to_string();
+                        println!("Author: {}", origin.author);
+                        println!("Message: {}", origin.message);
+                        println!("Commit: {}", origin.commit);
+                        println!("Time: {}", origin.time);
+                    }
+                }
+            }
+
+            // Print the remote URL
+            if let Ok(remote) = repo.find_remote("origin") {
+                if let Some(url) = remote.url() {
+                    origin.remote = url.to_owned();
+                }
+            }
+
+            // Check if the current commit is tagged
+            if let Ok(tag_name) = repo.describe(git2::DescribeOptions::new().describe_tags()) {
+                origin.tag = tag_name.format(None).unwrap_or_default();
+                // println!("Tagged Version: {}", tag_name.format(None).unwrap());
+            }
+
+            // Print the repository state (e.g., if it's clean, has uncommitted changes, etc.)
+            match repo.state() {
+                RepositoryState::Clean => origin.state = "clean".to_owned(),
+                _ => origin.state = format!("{:?}", repo.state()),
+            }
         }
+        origin
     }
 
-    /// Origin from directory
-    /// This is a fallback.
-    /// # Arguments
-    ///
-    /// * `dir` - plain fs directory.
-    ///
-    /// # Returns
-    ///
-    /// Origin
-    fn from_dir<P>(dir: P) -> std::io::Result<Origin>
-    where
-        P: AsRef<Path>,
-    {
-        match dir.as_ref().is_dir() {
-            true => Ok(Origin {
-                directory: dir.as_ref().canonicalize().unwrap(),
-                url: "no url".to_string(),
-                branch_nane: "".to_string(),
-                message: "".to_string(),
-                tag: "".to_string(),
-            }),
-            false => Err(std::io::Error::new(
-                // std::io::ErrorKind::NotADirectory,   // only in nightly as of 2023-06-30 // TODO: change as soon as it's in stable
-                std::io::ErrorKind::Other,
-                "not a directory",
-            )),
-        }
-    }
-
-    /// Origin from git directory
-    /// # Arguments
-    ///
-    /// * `git_dir` - directory of the git repo.
-    ///
-    /// # Returns
-    ///
-    /// Origin
-    fn from_git_dir<P>(git_dir: P) -> Result<Origin, git2::Error>
-    where
-        P: AsRef<Path>,
-    {
-        // Open the repository
-        let repo = Repository::open(&git_dir)?;
-
-        // Get the active branch
-        let branch = repo.head()?;
-        let branch_name = branch.shorthand().unwrap().to_string();
-
-        // Get the latest commit message
-        let latest_commit = branch.peel_to_commit()?;
-        let commit_message = latest_commit
-            .message()
-            .unwrap_or("no commit message")
-            .to_string();
-
-        // Get the remote url
-        let remote = repo.find_remote("origin")?;
-        let remote_url = remote.url().unwrap_or("no remote url").to_string();
-
-        Ok(Origin {
-            directory: repo.path().to_owned(),
-            url: remote_url,
-            branch_nane: branch_name,
-            message: commit_message,
-            tag: "no tag".to_string(),
-        })
-    }
-
-    /// write file `path/ORIGIN`
-    /// # Arguments
-    ///
-    /// * `path` - path to write the file to.
-    ///
-    /// # Returns
-    ///
-    /// std::io:Result()
-    ///
-    /// # Examples
-    /// ```
-    /// fn main() -> io::Result<()> {
-    ///     let git_origin = Origin::new("./");
-    ///     git_origin.write_origin_file("./")?;
-    /// }
-    /// ```
     pub fn write_origin_file<P>(&self, path: P) -> std::io::Result<()>
     where
         P: AsRef<Path>,
